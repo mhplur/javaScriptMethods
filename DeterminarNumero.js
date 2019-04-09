0d014eeb537b66de284ed7e0894058c539c1cdf4
Milton Paredes <mhplur@hotmail.com>
2019-04-09 15:08:20 -0500

    IMPRIME ETIQUETAS PARA CADA NUMERO
    IMPRIME ETIQUETAS PARA CADA NUMERO

--- DeterminarNumero.js
+++ DeterminarNumero.js
@@ -2,8 +2,9 @@
 	var positivos=0;
   	var negativos=0;
   	var ceros=0;
-  	var array=[-9,-8,0,98,0,5, -9589889, 9];
+  	var array=[-9,-8,0,98,0,5, -9589889, 9, 909, -8, 0, 89, 897, 0, 0];
   	var fracciones=[];
+    var mensajes=[];
 
   	for (var i=0 ; i<array.length ; i++)
 		if (array[i]<0)
@@ -14,5 +15,11 @@
         	positivos = positivos +1;
             
   	fracciones=[positivos,ceros,negativos];
-  	document.write(fracciones);
+    mensajes=["POSITIVOS: ","CEROS: ","NEGATIVOS: "];
+    for (var i=0 ; i<mensajes.length ; i++){
+    	document.write(mensajes[i] + fracciones[i] + "<br/>");
+   		
+    }
+    
+  	//document.write(fracciones);
 </script>
